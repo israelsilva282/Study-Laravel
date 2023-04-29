4fe14049764c2ea381dfb1b95f31742670b17675
israelsilva282 <israelcostas@hotmail.com>
2023-04-29 15:28:29 -0300

    Adicionando controllers

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\EventController;
+
+Route::get('/', [EventController::class, 'index']);
+
+Route::get('/events/create', [EventController::class, 'create']);
 
 Route::get('/produto/{id}', function ($id = null) {
     return view('product', ['id' => $id]);
