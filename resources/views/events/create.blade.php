6fd379426e369060f596a977217834c6861792ab
israelsilva282 <israelcostas@hotmail.com>
2023-05-01 16:29:02 -0300

    Upload de imagem

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -9,6 +9,10 @@
     <form action="/events" method="POST">
         @csrf
         <div class="form-group">
+            <label for="image">Imagem do Evento:</label>
+            <input type="file" id="image" name="image" class="from-control-file">
+        </div>
+        <div class="form-group">
             <label for="title">Evento:</label>
             <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
         </div>
