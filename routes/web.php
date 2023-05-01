7fb978fcc7796e1c7f8cb62853db40f83ed92659
israelsilva282 <israelcostas@hotmail.com>
2023-05-01 15:12:12 -0300

    Adicionado rota POST para criar evento

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\EventController;
 Route::get('/', [EventController::class, 'index']);
 
 Route::get('/events/create', [EventController::class, 'create']);
+Route::post('/events', [EventController::class, 'store']);
 
 Route::get('/produto/{id}', function ($id = null) {
     return view('product', ['id' => $id]);
