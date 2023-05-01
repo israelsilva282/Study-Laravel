255a18a29e68ce116de24a941319ac6ac8f7211a
israelsilva282 <israelcostas@hotmail.com>
2023-05-01 15:31:03 -0300

    Adicionado estrutura para exibição de mensagem

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -42,10 +42,20 @@
             </div>
         </nav>
     </header>
-    @yield('content')
+    <main>
+        <div class="container-fluid">
+            <div class="row">
+                @if(session('msg'))
+                <p class="msg">{{session('msg')}}</p>
+                @endif
+                @yield('content')
+            </div>
+        </div>
+    </main>
     <footer>
         <p>Study Laravel &copy; 2023</p>
     </footer>
+
 </body>
 
 </html>
